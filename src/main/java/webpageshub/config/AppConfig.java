fc64e88f6f308b40b25fca167a4fcdd88c4520a8
Jonas <bylomo@126.com>
2018-04-22 23:50:50 +0800

    integrate with front end

--- src/main/java/webpageshub/config/AppConfig.java
+++ src/main/java/webpageshub/config/AppConfig.java
@@ -14,7 +14,7 @@ public class AppConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/");
+                registry.addMapping("*").allowedOrigins("http://localhost:4200");
             }
         };
     }
